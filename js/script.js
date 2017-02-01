5aa1687bcacb2fe9bd9dca3201b472e364e0e3ea
Kyle Chivers <kchiv@Kyles-MacBook-Pro.local>
2017-01-31 21:49:14 -0800

    Added AJAX call for New York Times API

--- js/script.js
+++ js/script.js
@@ -23,6 +23,21 @@ function loadData() {
 
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    //NYT AJAX request
+
+    var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=09483b27be10410dbd95bd0f6de8913a';
+
+    $.getJSON(nytimesURL, function(data) {
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>'
+                + '<p>' + article.snippet + '</p>' + '</li>');
+        };
+    });
+
     return false;
 };
 
